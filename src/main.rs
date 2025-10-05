04cb2d12fe15e3925900b08fb629db358adc43a8
Elmer537 <epan@wisdome.ai>
2025-10-05 07:15:51 +0000

    chore: setup config

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use axum::{Router, routing::get};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 mod routers;
 mod services;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     // build our application with a single route
     let app = Router::new()
         .merge(routers::log_router())
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/health", get(|| async { "OK" }));
-    // .route("/logs", get(services::get_logs));
+        .route("/health", get(|| async { "OK" }))
+        .layer(TraceLayer::new_for_http());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
